b464d7be81d34510d08a8878733d08e13a7240ea
Sanio Luke Sebastian <93911177+SanioLukeSebastianMCA@users.noreply.github.com>
2022-04-05 12:59:11 +0530

    Add files via upload

---
+++ 05-04-2022/Vehicle.java
@@ -0,0 +1,17 @@
+class Vehicle{
+	String vname, vmodel;
+	int vserialno, vprice;
+	
+	Vehicle(){  }
+	
+	Vehicle(){
+		this.vname= vname;
+		this.vmodel= vmodel;
+		this.vserialno= vserialno;
+		this.vprice= vprice;
+	}
+}
+
+class VehicleDetails(){
+	
+}
